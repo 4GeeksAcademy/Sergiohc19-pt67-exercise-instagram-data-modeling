4522467509915033b21eb5c8ac96f3de90d42b9c
Sergiohc19 <161878354+Sergiohc19@users.noreply.github.com>
2024-07-01 23:16:47 +0000

    completado

--- src/models.py
+++ src/models.py
@@ -7,39 +7,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False) 
+
+
 class Post(Base):
     __tablename__ = 'post'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    likes = Column(String(250), nullable=False)
-    coments = Column(String(250), nullable=False)
-   
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
-
-class Usuarios(Base):
-    __tablename__ = 'usuarios'
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    password = Column(String(250), nullable=False)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 
-
-
-class Saved(Base):
-    __tablename__ = 'saved'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    post_relacion = Column(Integer, ForeignKey('post.id'),nullable= False)
-    usuario_relacion = Column(Integer, ForeignKey('usuarios.id'),nullable= False)
-    personas = relationship(Post)
-    usuarios= relationship(Usuarios)
-
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 
     def to_dict(self):
